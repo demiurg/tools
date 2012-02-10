bc45c0bb7206ae98b6e38a485e63d346b41dfc98
Pavel Dorovskoy <pavel@lenin.local>
2012-02-09 22:30:55 -0500

    Almost dont

--- csvs2sql.py
+++ csvs2sql.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os, sys, glob, csv
 
 files = glob.glob("./*.txt")
@@ -7,10 +9,24 @@ fields = []
 for f in files:
     data = csv.DictReader(open(f, 'r'))
     fields.append(set(data.fieldnames))
+    
+fields = set.intersection(*fields)
 
-i = set.intersection(*fields)
+query = "INSERT INTO geohealth_birds (%s) VALUES ('%s');"
+names = ", ".join(fields)
 
-data = csv
+count = 0
 for f in files:
     data = csv.DictReader(open(f, 'r'))
-    fields.append(set(data.fieldnames))
+    for row in data:
+        values = []
+        
+        for field in fields:
+            values.append(row[field])
+            
+        values = "', '".join(values)
+        print query % (names, values)
+        #print values
+        count += 1
+        
+#print "Number of records: "+count 
